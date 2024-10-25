6682ada448c8990204094623c72ca714fa46ff03
Andrew Buschmann <andrew.buschmann@epicgames.com>
2024-10-25 12:18:28 -0400

    Initial commit for the csharp folder

---
+++ SdetBootcampDay1/Exercises/Exercises02.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using SdetBootcampDay1.TestObjects;
+
+namespace SdetBootcampDay1.Exercises
+{
+    [TestFixture]
+    public class Exercises02
+    {
+        /**
+         * TODO: rewrite these three tests into a single, parameterized test.
+         * You decide if you want to use [TestCase] or [TestCaseSource], but
+         * I would like to know why you chose the option you chose afterwards.
+         */
+        [TestCase(1000, 500, 500, TestName = "Deposit 1000 then withdraw 500 equals 500")]
+        [TestCase(1000, 1000, 0, TestName = "Deposit 1000 then withdraw 1000 equals 0")]
+        [TestCase(2500, 1, 2499, TestName = "Deposit 2500 then withdraw 1 equals 2499")]
+        public void SavingsAccount_DepositThenWithdraw_BalanceShouldBeUpdatedAccordingly
+            (int amountToDeposit, int amountToWithdraw, int expectedBalance)
+        {
+            var account = new Account(AccountType.Savings);
+
+            account.Deposit(amountToDeposit);
+            account.Withdraw(amountToWithdraw);
+
+            Assert.That(account.Balance, Is.EqualTo(expectedBalance));
+        }
+    }
+}
---
+++ SdetBootcampDay1/Exercises/Exercises03.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using SdetBootcampDay1.TestObjects;
+
+namespace SdetBootcampDay1.Exercises
+{
+    [TestFixture]
+    public class Exercises03
+    {
+        [Test]
+        public void TryingToOverdrawOnASavingsAccountThrowsExpectedException()
+        {
+            /**
+             * TODO: Create a new savings account and deposit 50.
+             * Verify that attempting to withdraw 100 throws an ArgumentException
+             * Also verify the exception message to be 'You cannot overdraw on a savings account'
+             * Finally, verify that the account balance is unchanged (i.e., you still have $50)
+             */
+
+             // Creates a new account with type of Savings
+            var account = new Account(AccountType.Savings);
+
+            // Deposits 50 into the Savings account
+            account.Deposit(50);
+
+            // Throws an Argument Excecption when attempting to withdraw more than the account balance
+            var aexp = Assert.Throws<ArgumentException>(() =>
+            
+            // Withdraw 100 from the savings account
+            {
+                account.Withdraw(100);
+            });
+
+            // Tests that the correct string is thrown when encountering this exception
+            Assert.That(aexp.Message, Is.EqualTo("You cannot overdraw on a savings account"));
+            
+            // Tests that the account balance is still 50
+            Assert.That(account.Balance, Is.EqualTo(50));
+        }
+    }
+}
